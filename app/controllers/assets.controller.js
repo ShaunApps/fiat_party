402f8b902151d2b4b923c8751234fd66b10b110f
ShaunApps <shaun.apps@gmail.com>
2017-01-14 11:55:28 -0500

    matt just got here

--- app/controllers/assets.controller.js
+++ app/controllers/assets.controller.js
@@ -7,36 +7,41 @@ module.exports = {
 
 
   var assets = [
-    { name: 'XCP', value: '5'},
-    { name: 'RAREPEPE', value: '3'},
-    { name: 'XUSD', value: '100'}
+    { name: 'XCP', amount: '5'},
+    { name: 'RAREPEPE', amount: '3'},
+    { name: 'XUSD', amount: '100'}
   ]
 
   //   var request = require('request');
   //
   //
   //
-  //   var assets = [];
-  //   var names = {};
+  //   const assets = [];
   //
   //
-  //   var makeRequest = () => {
+  //   var makeRequest = (assets) => {
   //     var address = "1FwkKA9cqpNRFTpVaokdRjT9Xamvebrwcu";
   //     var url = "https://counterpartychain.io/api/balances/" + address;
   //
   //     request(url, function (error, response, body) {
-  //     if (!error && response.statusCode == 200) {
-  //       var body = JSON.parse(body);
-  //       var data = body["data"];
-  //       for (var i = 0; i < data.length; i++) {
-  //         names.name = data[i]["asset"];
-  //         assets.push(names);
+  //       if (!error && response.statusCode == 200) {
+  //         var body = JSON.parse(body);
+  //         var data = body["data"];
+  //         for (var i = 0; i < data.length; i++) {
+  //           var assetname = data[i]["asset"];
+  //           var assetamount = data[i]["amount"];
+  //           var newobject = {};
+  //           newobject.name = assetname;
+  //           newobject.amount = assetamount;
+  //           assets.push(newobject);
+  //         }
+  //
   //       }
-  //       console.log(assets);
-  //     }
   //   })
+  //
   // }
-  //   makeRequest();
+  //   makeRequest(assets);
+  //
 
 
     res.render('pages/assets', { assets: assets });
