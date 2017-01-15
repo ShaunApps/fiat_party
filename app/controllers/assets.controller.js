fb9062b4b0966d1e110094f09cfb2086d1804038
ShaunApps <shaun.apps@gmail.com>
2017-01-14 22:42:09 -0500

    added temp fix for assets where name != id

--- app/controllers/assets.controller.js
+++ app/controllers/assets.controller.js
@@ -1,61 +1,61 @@
-
 const request = require('request');
 const priceassets = require('./priceassets.controller');
+const Promise = require('bluebird');
 
 module.exports = {
 
-  presentAssets: (req, res) => {
-
-  // var assets = [
-  //   { name: 'XCP', amount: '5'},
-  //   { name: 'RAREPEPE', amount: '3'},
-  //   { name: 'XUSD', amount: '100'}
-  // ]
+    presentAssets: (req, res) => {
 
 
-
-
-    var assets = [];
-
-
-
-    var makeRequest = () => {
       var address = req.body.assetaddress;
       var url = "https://counterpartychain.io/api/balances/" + address;
 
 
-
       function callback(error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var body = JSON.parse(body);
-          var data = body["data"];
+          if (!error && response.statusCode == 200) {
+              var assets = [];
+              var body = JSON.parse(body);
+              var data = body["data"];
 
-          for (var i = 0; i < data.length; i++) {
-            var assetname = data[i]["asset"];
-            var assetamount = data[i]["amount"];
-            var newobject = {};
-            newobject.name = assetname;
-            newobject.amount = assetamount;
-            assets.push(newobject);
+              for (var i = 0; i < data.length; i++) {
+                  var assetname = data[i]["asset"];
+                  var assetamount = data[i]["amount"];
+                  var newobject = {};
+                  newobject.name = assetname;
+
+                  switch (assetname) {
+                    case "XCP":
+                      newobject.name = "Counterparty";
+                      break;
+                    case "SJCX":
+                      newobject.name = "Storjcoin X";
+                      break;
+                    case "PEPECASH":
+                      newobject.name = "pepe-cash";
+                      break;
+                    case "GEMZ":
+                      newobject.name = "gems";
+                      break;
+                    case "FLDC":
+                      newobject.name = "foldingcoin";
+                      break;
+                    default:
+                      newobject.name = assetname;
+                  }
+
+                  newobject.amount = assetamount;
+                  assets.push(newobject);
+              }
           }
-          test_function(assets);
-            // res.render('pages/assets', { assets: assets });
-            // var assets_with_prices = priceassets.get_USD_AMNT(assets);
-            // console.log(assets_with_prices);
-        }
 
+        priceassets.get_USD_AMNT(assets);
+
+          // return assets;
       }
-      request(url, callback);
+
+
+      request(url, callback)
+
     }
-
-    makeRequest()
-
-
-
-
-
-
-  }
-
 
 }
