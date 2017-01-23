c16115ead94fd897cbf67e392bce24ee6dadccdc
ShaunApps <shaun.apps@gmail.com>
2017-01-23 14:41:53 -0500

    trying something different here, but still hitting issues

--- app/controllers/assets.controller.js
+++ app/controllers/assets.controller.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const rp = require('request-promise');
 const priceassets = require('./priceassets.controller');
 
 
@@ -7,59 +8,79 @@ module.exports = {
     presentAssets: (req, res) => {
 
 
-      var address = req.body.assetaddress;
-      var url = "https://counterpartychain.io/api/balances/" + address;
+        var address = req.body.assetaddress;
+        var url = "https://counterpartychain.io/api/balances/" + address;
 
 
-      request({
-        method: 'GET',
-        url: url,
-      },
-       function (error, response, body) {
-         if (!error && response.statusCode == 200) {
-           var assets = [];
-           var body = JSON.parse(body);
-           var data = body["data"];
+        request({
+                method: 'GET',
+                url: url,
+            },
+            function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var assets = [];
+                    var body = JSON.parse(body);
+                    var data = body["data"];
+                    var url;
 
-           for (var i = 0; i < data.length; i++) {
-               var assetname = data[i]["asset"];
-               var assetamount = data[i]["amount"];
-               var newobject = {};
-
-
-               // handle case where asset name != coinmarketcap name 
-               switch (assetname) {
-                 case "XCP":
-                   newobject.name = "Counterparty";
-                   break;
-                 case "SJCX":
-                   newobject.name = "Storjcoin X";
-                   break;
-                 case "PEPECASH":
-                   newobject.name = "pepe-cash";
-                   break;
-                 case "GEMZ":
-                   newobject.name = "gems";
-                   break;
-                 case "FLDC":
-                   newobject.name = "foldingcoin";
-                   break;
-                 default:
-                   newobject.name = assetname;
-               }
-
-               newobject.amount = assetamount;
-               assets.push(newobject);
-           }
-           return assets;
-         }
-      })
+                    for (var i = 0; i < data.length; i++) {
+                        var assetname = data[i]["asset"];
+                        var assetamount = data[i]["amount"];
+                        var newobject = {};
 
 
-      res.render('pages/assets', {assets: assets});
+                        // handle case where asset name != coinmarketcap name
+                        switch (assetname) {
+                            case "XCP":
+                                newobject.name = "Counterparty";
+                                break;
+                            case "SJCX":
+                                newobject.name = "Storjcoin X";
+                                break;
+                            case "PEPECASH":
+                                newobject.name = "pepe-cash";
+                                break;
+                            case "GEMZ":
+                                newobject.name = "gems";
+                                break;
+                            case "FLDC":
+                                newobject.name = "foldingcoin";
+                                break;
+                            default:
+                                newobject.name = assetname;
+                        }
+
+                        newobject.amount = assetamount;
+                        priceassets.return_USD_AMNT(newobject);
+
+                        url = "https://api.coinmarketcap.com/v1/ticker/" + newobject.name.toLowerCase();
+                        var options = {
+                            uri: url
+                        }
+                        rp(options)
+                            .then(function(body) {
+                                var data1 = JSON.parse(body);
+                                console.log(data1);
+                                if (!data1["error"]) {
+                                    newobject.unit_price = data1[0].price_usd;
+                                }
+                                assets.push(newobject);
+                                console.log(assets);
+                            })
+                            .catch(function (err) {
+                              // console.log("request failed");
+                            });
+
+                        //  assets.push(USD_object);
+                        // console.log(USD_object);
+
+                    }
+                }
+            })
 
 
 
+        // res.render('pages/assets', {assets: assets});
 
 
     }
--- app/controllers/priceassets.controller.js
+++ app/controllers/priceassets.controller.js
@@ -1,75 +1,103 @@
 const request = require('request');
-// const alsoHelper = require('./also.helper.controller');
-const Foo = require('./also.helper.controller');
-const Promise = require('promise');
-var async = require("async");
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const helperpriceassets = require('./priceassets.helper.controller');
+const request_then = require('then-request');
+
 
 
 module.exports = {
 
-    get_USD_AMNT: (assets) => {
+    return_USD_AMNT: (assetObject) => {
 
 
-      // function anAsyncCall() {
-      //   var promise = doSomethingAsync();
-      //   promise.then(function() {
-      //       somethingComplicated();
-      //   });
-      //
-      //   return promise;
-      //   }
-        var some_array = [];
-        function workMyCollection(arr) {
-
-          return Promise.all(arr.map(function(item) {
-            var url = "https://api.coinmarketcap.com/v1/ticker/" + assets[item].name.toLowerCase();
-              return request(url, function(error, response, body) {
-                  if (!error) {
-                      var data = JSON.parse(body);
-                      if (!data["error"]) {
-                          var name = data[0].name;
-                          var unit_price = data[0].price_usd;
-                          var asset_amount = assets[i].amount;
-                          some_array.push({
-                              name: name,
-                              unit_price: unit_price,
-                              asset_amount: asset_amount
-                          });
-                      }
-                  };
-                });
-              }))
-            }
-        workMyCollection(assets);
-        console.log(some_array);
+        // var url = "https://api.coinmarketcap.com/v1/ticker/" + assetObject.name.toLowerCase();
+        // var amount = parseFloat(assetObject.amount);
+        //
+        //
+        //
+        // request(url, function(error, response, body) {
+        //     if (!error) {
+        //         var data = JSON.parse(body);
+        //         if (!data["error"]) {
+        //             var name = data[0].name;
+        //             var unit_price = data[0].price_usd;
+        //             var asset_amount = assets[i].amount;
+        //             some_array.push({
+        //                 name: name,
+        //                 unit_price: unit_price,
+        //                 asset_amount: asset_amount
+        //             });
+        //         }
+        //     }
+        // })
 
 
 
+        // request_then('GET', url, function(err, res) {
+        //     if (!err) {
+        //         var data = JSON.parse(res.body);
+        //         if (!data["error"]) {
+        //             var name = data[0].name;
+        //             var unit_price = data[0].price_usd;
+        //             var asset_amount = amount;
+        //             return {
+        //                 name: name,
+        //                 unit_price: unit_price,
+        //                 asset_amount: asset_amount
+        //             };
+        //         }
+        //     }
+        // })
 
-    //     for (let i = 0; i < assets.length; i++) {
-    //
-    //         var url = "https://api.coinmarketcap.com/v1/ticker/" + assets[i].name.toLowerCase();
-    //         var amount = parseFloat(assets[i].amount);
-    //
-    //         request(url, function(error, response, body) {
-    //             if (!error) {
-    //                 var data = JSON.parse(body);
-    //                 if (!data["error"]) {
-    //                     var name = data[0].name;
-    //                     var unit_price = data[0].price_usd;
-    //                     var asset_amount = assets[i].amount;
-    //                     some_array.push({
-    //                         name: name,
-    //                         unit_price: unit_price,
-    //                         asset_amount: asset_amount
-    //                     });
-    //                 }
-    //             }
-    //
-    //         })
-    //     }
-    //     callback(some_array);
-  }
+
+
+
+        //
+        // function workMyCollection(arr, some_array) {
+        //   return Promise.all(arr.map(function(item) {
+        //     var url = "https://api.coinmarketcap.com/v1/ticker/" + arr[item].name.toLowerCase();
+        //       return request(url, function(error, response, body) {
+        //           if (!error) {
+        //               var data = JSON.parse(body);
+        //               if (!data["error"]) {
+        //                   var name = data[0].name;
+        //                   var unit_price = data[0].price_usd;
+        //                   var asset_amount = arr[i].amount;
+        //                   some_array.push({
+        //                       name: name,
+        //                       unit_price: unit_price,
+        //                       asset_amount: asset_amount
+        //                   });
+        //               }
+        //           };
+        //         });
+        //       }))
+        //     }
+        // console.log(workMyCollection(assets, some_array));
+
+
+        // assets.forEach(function(i) {
+        //   var url = "https://api.coinmarketcap.com/v1/ticker/" + assets[i].name.toLowerCase();
+        //   var amount = parseFloat(assets[i].amount);
+        //
+        //     request(url, function(error, response, body) {
+        //         if (!error) {
+        //             var data = JSON.parse(body);
+        //             if (!data["error"]) {
+        //                 var name = data[0].name;
+        //                 var unit_price = data[0].price_usd;
+        //                 var asset_amount = assets[i].amount;
+        //                 some_array.push({
+        //                     name: name,
+        //                     unit_price: unit_price,
+        //                     asset_amount: asset_amount
+        //                 });
+        //             }
+        //         }
+        //     })
+        //     console.log(some_array);
+        // })
+
+
+    }
 
 }
